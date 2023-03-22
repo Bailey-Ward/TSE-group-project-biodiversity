20cbb7b65fbdc34a319eb9ef89cb0994f8cd19f7
Bailey Ward <25754670@students.lincoln.ac.uk>
2023-03-22 11:36:10 +0000

    a

--- fileReader.py
+++ fileReader.py
@@ -5,12 +5,7 @@ class dataSetReader:
     def fileRead(fileLocation):
 
         try: #geopandas library is used to read .TAB and associated files
-<<<<<<< HEAD
-            gdf = geopandas.read_file(fileLocation) #gdf stands for GeoDataFrame
+            gdf = gpd.read_file(fileLocation) #gdf stands for GeoDataFrame
             print(gdf)
-=======
-            tabFile = gpd.read_file(fileLocation)
-            return tabFile
->>>>>>> 2ba7c2e4bb992614292a8f1456f8752b32eabf4b
         except: #if file is not found, error message is passed
             print("File could not be found")
