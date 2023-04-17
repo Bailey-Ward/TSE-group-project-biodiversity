18d7075a85dcf0d237618d89a225daca3761fe71
Bailey Ward <25754670@students.lincoln.ac.uk>
2023-04-17 16:09:50 +0100

    insert data working properly now

--- database.py
+++ database.py
@@ -1,40 +1,56 @@
-import psycopg2
-import pandas as pd
-import glob as glob
 import geopandas as gpd
+import pandas as pd
+import glob
 from sqlalchemy import create_engine
 import geoalchemy2
-import fiona 
-
-fiona.supported_drivers
+import psycopg2
 
 class database:
 
     def insertData(fileLocation):
+        
 
         engine = create_engine("postgresql://postgres:lincolnBio@localHost:5432/lincolnBiodiversity")
 
         #geopandas library is used to read .TAB and associated files
-        tabFile = gpd.read_file("{}.tab".format(fileLocation))
-        print(tabFile)
+        try:
+            allTabFile = glob.glob("{}/*.tab".format(fileLocation))
+            print(f"found {len(allTabFile)} files: {allTabFile}")
+        except:
+            print("No files were found in this directory")
 
-        gdf = gpd.GeoDataFrame(tabFile)
+        gdfs = []
 
-        gdf.to_file("{}.shp".format(fileLocation), index=False, driver= "ESRI Shapefile")
-    
-        shpFile = gpd.read_file("{}.shp".format(fileLocation))
-        
-        gdf = gpd.GeoDataFrame(shpFile)
+        try:
+            for filename in allTabFile:
+                gdf = gpd.read_file(filename)
+                gdfs.append(gdf)
+                print(f"Successfully read {filename} into a geoDataFrame")
+        except:
+            print(f"Error reading {filename} into a geoDataFrame")
 
-        gdf.to_postgis(name="sites", con = engine, if_exists="append", index=False) #adds items to the DB (not working as intended yet)
+        try:
+            merged_gdf = gpd.GeoDataFrame(pd.concat(gdfs,ignore_index=True)).to_crs("EPSG:4326").set_index('Site_ID')
+        except:
+            print("No files were found in the directory")
+
+        try:
+            merged_gdf.to_postgis(name="sites", con = engine, if_exists="replace", index=True) #adds items to the DB (not working as intended yet)
+            print("Items added to the database")
+        except:
+            print("Items were not added to the database")
+
 
 
     def displayData():
-        engine = create_engine("postgresql://postgres:lincolnBio@localHost:5432/lincolnBiodiversity")
+        try:
+            engine = create_engine("postgresql://postgres:lincolnBio@localHost:5432/lincolnBiodiversity")
 
-        query = "SELECT * FROM sites"
-        gdf = gpd.GeoDataFrame.from_postgis(query, con=engine, geom_col="geometry")
-        print(gdf) #logic for printing the DB
+            query = "SELECT * FROM sites"
+            gdf = gpd.GeoDataFrame.from_postgis(query, con=engine, geom_col="geometry")
+            print(gdf) #logic for printing the DB
+        except:
+            print("Database could not be connected to")
 
 
     def removeData(siteID):
--- pythonDataApp.py
+++ pythonDataApp.py
@@ -5,32 +5,49 @@ import os
 #main function and basic menu
 if __name__ == '__main__':
 
+    try:
+        while True:
+            userChoice = input("Type 1 to add a file to the database)\nType 2 to remove a site from the database)\nType 3 to display all data from the DB)\nType 4 to exit the program)")
+            try:
+                userChoice = int(userChoice)
+            except ValueError:
+                print("Input should be a number between 1 and 4")
+                continue
 
-    while True:
-        userChoice = int(input("Type 1 to add a file to the database)\nType 2 to remove a site from the database)\nType 3 to display all data from the DB)\nType 4 to exit the program)"))
+            if userChoice == 1:
+                fileLocation = (input("Enter the name of the directory you wish to read .tab files from: (Example: C:/TSE_datasets)"))
+                try:
+                    database.insertData(fileLocation)
+                except FileNotFoundError:
+                    print("File was not found.")
+                    continue
+                except IOError:
+                    print("Error reading file.")
+                    continue
+                #files will be read in and added to the DB using this function
 
-        if userChoice == 1:
-            fileLocation = (input("Enter the name of the file you wish to read (Example: C:/TSE datasets/CoL_LGS_21-22.tab)"))
-            database.insertData(fileLocation)
-            #files will be read in and added to the DB using this function
+            elif userChoice == 2:
+                siteID = int(input("Enter the site number for the site you wish to remove:"))
+                database.removeData(siteID) 
+                #entries can be selected for deletion from the DB here
 
-        elif userChoice == 2:
-            siteID = int(input("Enter the site number for the site you wish to remove:"))
-            database.removeData(siteID) 
-            #entries can be selected for deletion from the DB here
+            elif userChoice == 3:
+                database.displayData()
+                #prints everything currently in the DB
+                
+            elif userChoice == 4:
+                print("closing down")
+                break
 
-        elif userChoice == 3:
-            database.displayData()
-            #prints everything currently in the DB
-            
-        elif userChoice == 4:
-            print("closing down")
-            break
-
-        else:
-            print("Invalid input")
-
+            else:
+                print("Invalid input")
+                
+    except Exception as e:
+        print("An error has occurred:", e)
         
+        
+
+            
 
     
 #This project was the combined work of the Biodiversity Net Gain Data Integration team software engineering group at UOL
