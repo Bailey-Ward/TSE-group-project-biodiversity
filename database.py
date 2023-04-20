9fa096a02294a3fd968537c4e1ab6d27efeeaca2
Bailey Ward <25754670@students.lincoln.ac.uk>
2023-04-20 17:48:43 +0100

    final version prior to testing

--- database.py
+++ database.py
@@ -9,36 +9,35 @@ class database:
 
     def insertData(fileLocation):
         
-
         engine = create_engine("postgresql://postgres:lincolnBio@localHost:5432/lincolnBiodiversity")
 
         #geopandas library is used to read .TAB and associated files
         try:
-            allTabFile = glob.glob("{}/*.tab".format(fileLocation))
+            allTabFile = glob.glob("{}/*.tab".format(fileLocation))     #glob library is used to select all files in the directory which end with .tab
             print(f"found {len(allTabFile)} files: {allTabFile}")
         except:
-            print("No files were found in this directory")
+            print("No files were found in this directory.")
 
         gdfs = []
 
         try:
-            for filename in allTabFile:
+            for filename in allTabFile:     #each file is read in to a geodataframe and then appended to the list
                 gdf = gpd.read_file(filename)
                 gdfs.append(gdf)
-                print(f"Successfully read {filename} into a geoDataFrame")
+                print(f"Successfully read {filename} into a geoDataFrame.")
         except:
-            print(f"Error reading {filename} into a geoDataFrame")
+            print(f"Error reading {filename} into a geoDataFrame.")
 
         try:
-            merged_gdf = gpd.GeoDataFrame(pd.concat(gdfs,ignore_index=True)).to_crs("EPSG:4326").set_index('Site_ID')
+            merged_gdf = gpd.GeoDataFrame(pd.concat(gdfs,ignore_index=True)).to_crs("EPSG:4326").set_index('Site_ID')       #each geodataframe in the list is concatinated, set to a co-ordinate reference system of EPSG:4326 and indexed via Site_ID
         except:
-            print("No files were found in the directory")
+            print("No files were found in the directory.")
 
         try:
-            merged_gdf.to_postgis(name="sites", con = engine, if_exists="replace", index=True) #adds items to the DB (not working as intended yet)
-            print("Items added to the database")
+            merged_gdf.to_postgis(name="sites", con = engine, if_exists="replace", index=True)      #The concatinated geodataframe is then added to postGIS, the table sites is created, or if it exists it is replaced by a new table
+            print("Items added to the database.")
         except:
-            print("Items were not added to the database")
+            print("Items were not added to the database.")
 
 
 
@@ -47,24 +46,36 @@ class database:
             engine = create_engine("postgresql://postgres:lincolnBio@localHost:5432/lincolnBiodiversity")
 
             query = "SELECT * FROM sites"
-            gdf = gpd.GeoDataFrame.from_postgis(query, con=engine, geom_col="geometry")
-            print(gdf) #logic for printing the DB
+            gdf = gpd.GeoDataFrame.from_postgis(query, con=engine, geom_col="geometry")     #database is read into a geodataframe, then printed to the console
+            print(gdf) 
         except:
-            print("Database could not be connected to")
+            print("Unable to display the database.")
 
 
     def removeData(siteID):
+        
+            conn = psycopg2.connect(database='lincolnBiodiversity',user='postgres',password='lincolnBio')       #psycopg2 library used for connection here
 
-        engine = create_engine("postgresql://postgres:lincolnBio@localHost:5432/lincolnBiodiversity")
+            cur = conn.cursor()     #cursor object is created for accessing the DB
 
-        query = f"DELETE FROM sites WHERE sites.Site_ID = {siteID}" 
+            cur.execute(f"SELECT COUNT(*) FROM public.sites WHERE \"Site_ID\" = '{siteID}'")        #SQL statement counts how many rows contain the site id input, if 0, the user is informed that the DB does not contain that siteID
+            row_count = cur.fetchone()[0]
 
-        with engine.connect() as conn:
-            conn.execute(query)
+            if row_count == 0:
+                print(f"Site {siteID} does not exist in the DB.")
+                
+            else:
+                choice = input(f"Are you sure you want to delete site ID {siteID} from the database? y/n:\t").lower()
+                if choice == 'y':
+                    cur.execute(f"DELETE FROM public.sites WHERE \"Site_ID\" = '{siteID}'")     #If the siteID exists in the database, the function removes it and gives a confirmation message to the user
 
-        gdf = gpd.GeoDataFrame.from_postgis(query, con=engine, geom_col="geometry")
+                    conn.commit()
+                    cur.close()
+                
+                    print(f"Site {siteID} was successfully removed from the database.")
 
-        gdf #removing records from the DB (also not working as intended yet)
-
-
-
+                elif choice =='n':
+                    print(f"SiteID {siteID} was not deleted.")
+                    
+                else:
+                    print("Input must be a y/n.")
--- pythonDataApp.py
+++ pythonDataApp.py
@@ -1,21 +1,19 @@
 from database import *
-import glob as glob
-import os
 
 #main function and basic menu
 if __name__ == '__main__':
 
     try:
         while True:
-            userChoice = input("Type 1 to add a file to the database)\nType 2 to remove a site from the database)\nType 3 to display all data from the DB)\nType 4 to exit the program)")
+            userChoice = input("Type 1 to add a file to the database)\nType 2 to remove a site from the database)\nType 3 to display all data from the DB)\nType 4 to exit the program\nInput:)")
             try:
                 userChoice = int(userChoice)
             except ValueError:
-                print("Input should be a number between 1 and 4")
+                print("Input should be a number between 1 and 4.")
                 continue
 
             if userChoice == 1:
-                fileLocation = (input("Enter the name of the directory you wish to read .tab files from: (Example: C:/TSE_datasets)"))
+                fileLocation = (input("Enter the name of the directory you wish to read .tab files from (Example: C:/TSE_datasets):\t"))
                 try:
                     database.insertData(fileLocation)
                 except FileNotFoundError:
@@ -27,28 +25,25 @@ if __name__ == '__main__':
                 #files will be read in and added to the DB using this function
 
             elif userChoice == 2:
-                siteID = int(input("Enter the site number for the site you wish to remove:"))
+                siteID = input("Enter the site number for the site you wish to remove:")
                 database.removeData(siteID) 
                 #entries can be selected for deletion from the DB here
 
             elif userChoice == 3:
-                database.displayData()
-                #prints everything currently in the DB
-                
+                try:
+                    database.displayData()      #prints everything currently in the DB
+                except:
+                    print("Error accessing the database.")
+
             elif userChoice == 4:
-                print("closing down")
+                print("Closing down")
                 break
 
             else:
-                print("Invalid input")
+                print("Input should be a number between 1 and 4.\n")
                 
     except Exception as e:
         print("An error has occurred:", e)
         
-        
-
-            
-
-    
 #This project was the combined work of the Biodiversity Net Gain Data Integration team software engineering group at UOL
     
